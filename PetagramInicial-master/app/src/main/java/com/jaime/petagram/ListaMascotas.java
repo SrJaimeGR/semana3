e83b715505f4ce8ed7f63f45ffd0e95fc98a76f2
SrJaimeGR <116917853+SrJaimeGR@users.noreply.github.com>
2022-11-10 22:12:40 -0500

    Add files via upload

---
+++ PetagramInicial-master/app/src/main/java/com/jaime/petagram/ListaMascotas.java
@@ -0,0 +1,81 @@
+package com.miislyk.petagram;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+public class ListaMascotas extends AppCompatActivity {
+
+    ArrayList<Mascota> mascotas;
+    private RecyclerView listaMascotas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_mascotas);
+
+        Toolbar actionBar = (Toolbar) findViewById(R.id.actionbar);
+        setSupportActionBar(actionBar);
+
+        listaMascotas = (RecyclerView) findViewById(R.id.recyclerListaMascotas);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        listaMascotas.setLayoutManager(linearLayoutManager);
+        inicializarListaMascotas();
+        inicializarAdaptador();
+
+    }
+
+    public void inicializarAdaptador(){
+
+        MascotaAdaptador adaptador = new MascotaAdaptador(mascotas, this);
+        listaMascotas.setAdapter(adaptador);
+
+    }
+
+    public void inicializarListaMascotas(){
+
+        mascotas = new ArrayList<Mascota>();
+
+        mascotas.add(new Mascota(R.drawable.fox, "Kira"));
+        mascotas.add(new Mascota(R.drawable.bear, "Nero"));
+        mascotas.add(new Mascota(R.drawable.elephant, "Lola"));
+        mascotas.add(new Mascota(R.drawable.sheep, "Gardfiel"));
+        mascotas.add(new Mascota(R.drawable.monkey, "Simon"));
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.mAbout:
+
+                break;
+            case R.id.mFavoritos:
+                Intent i = new Intent(this, MascotasFavoritas.class);
+                startActivity(i);
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ PetagramInicial-master/app/src/main/java/com/jaime/petagram/MascotasFavoritas.java
@@ -0,0 +1,68 @@
+package com.miislyk.petagram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import java.util.ArrayList;
+
+public class MascotasFavoritas extends AppCompatActivity {
+
+    ArrayList<Mascota> mascotasFAV;
+    private RecyclerView listaMascotasFAV;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mascotas_favoritas);
+
+        Toolbar actionBar = (Toolbar) findViewById(R.id.actionbar);
+        setSupportActionBar(actionBar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        listaMascotasFAV = (RecyclerView) findViewById(R.id.recyclerListaMascotasFAV);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        listaMascotasFAV.setLayoutManager(linearLayoutManager);
+        inicializarListaMascotas();
+        inicializarAdaptador();
+
+
+    }
+
+    public void inicializarAdaptador(){
+
+        MascotaAdaptador adaptador = new MascotaAdaptador(mascotasFAV, this);
+        listaMascotasFAV.setAdapter(adaptador);
+
+    }
+
+    public void inicializarListaMascotas(){
+
+        mascotasFAV = new ArrayList<Mascota>();
+
+        mascotasFAV.add(new Mascota(R.drawable.fox, "Kira"));
+        mascotasFAV.add(new Mascota(R.drawable.bear, "Nero"));
+        mascotasFAV.add(new Mascota(R.drawable.elephant, "Lola"));
+        mascotasFAV.add(new Mascota(R.drawable.sheep, "Gardfiel"));
+        mascotasFAV.add(new Mascota(R.drawable.monkey, "Simon"));
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_activity_2, menu);
+        return true;
+
+    }
+
+}
